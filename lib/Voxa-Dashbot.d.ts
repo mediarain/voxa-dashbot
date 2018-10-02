85cc071af54e6cebdafff27efafe4239aa675d47
Andrés Reyes Monge <armonge@gmail.com>
2018-10-02 15:36:56 -0600

    Updating tests

--- lib/Voxa-Dashbot.d.ts
+++ lib/Voxa-Dashbot.d.ts
@@ -1,10 +1,10 @@
 import { VoxaApp } from "voxa";
 export interface IVoxaDashbotConfig {
-    debug?: boolean;
-    suppressSending?: boolean;
-    api_key?: string;
     alexa?: string;
-    dialogflow?: string;
+    api_key?: string;
     botframework?: string;
+    debug?: boolean;
+    dialogflow?: string;
+    suppressSending?: boolean;
 }
 export declare function register(skill: VoxaApp, config: IVoxaDashbotConfig): void;
